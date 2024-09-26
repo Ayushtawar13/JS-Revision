1d6945a73c86ca0ff7738cb94aced2c0e5519a31
Ayushtawar13 <aayushtanwar2003@gmail.com>
2024-09-26 20:03:22 +0530

    third commit

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+// "use strict " // treat all js code as newer version
+
+// if you want to execute 2 statement s in one line you should use ; 
+// console.log("ayush") console.log("1") 
+
+let name = "ayush"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+console.log(typeof(null));
+
+console.log(typeof(undefined));
+
