40975f8d54042a13df4d8f922597db0a293f0e61
Ayushtawar13 <aayushtanwar2003@gmail.com>
2024-09-29 14:59:38 +0530

    commit

---
+++ Basics/07_numbers_math.js
@@ -0,0 +1,50 @@
+
+let num = 100;
+// console.log(num);
+
+// creating number using object 
+
+let n = new Number(400);
+// console.log(n);
+
+/*Number of digits after the decimal point. Must be in the range 0 - 20, inclusive.
+Returns a string representing a number in fixed-point notation.*/ 
+
+n.toFixed(1)
+// console.log(n.toFixed(2));
+
+// console.log(n.toString()); // converts number into string
+
+// console.log(n.toString().length);
+
+/*Converts a number to  a string by using the current or specified locale.*/
+const rs =2335630000;
+// console.log(rs.toLocaleString('en-IN')); // indian rupees ke hisab se , lga dega
+
+const f=23.45675;
+// console.log(f.toPrecision(3)); //precise number print krta hai jese round off
+
+
+//  math   ----------------------------------------------------------------------------------------------------------------------------------
+
+const math = 30498.30954;
+console.log(Math.round(math));
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random()); // math.random gives values between 0 and 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 1
+const max = 6
+
+// below line gives values between 1 to 6 
+console.log("1 to 6");
+console.log(Math.floor((Math.random()*((max-min)) + min)));
---
+++ Basics/08_dates.js
@@ -0,0 +1,33 @@
+
+// date is a object in javascript 
+
+const date = new Date();
+console.log(date);
+console.log(date.toString());
+console.log(date.toLocaleString());
+console.log(date.toDateString());
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+// detailed customization
+ 
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
