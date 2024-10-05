cb2f6a44e85f8ce437128f03728f94236232b168
Ayushtawar13 <aayushtanwar2003@gmail.com>
2024-10-05 12:18:21 +0530

    commit arrays

--- Basics_02/arrays_02.js
+++ Basics_02/arrays_02.js
@@ -2,7 +2,66 @@ let arr = [1,3,5,6,7,8];
 
 let arr2 = [10,30,50,60,70,80];
 
-let arr3 = arr2.push(arr);
+// arr2.push(arr); // returns new length of array
+// console.log(arr2);
 
-console.log(arr);
-console.log(arr3);
+// flatens the pushed array 
+
+// let set = arr2.flat(Infinity); //returns a new array 
+// console.log(set);
+// console.log(arr2);
+
+// concat joining two arrays
+
+// let cb = arr.concat(arr2); returns a new array does not made any changes in arrays given 
+// console.log(cb);
+// console.log(arr2);
+// console.log(arr);
+
+
+// spread operator ;- returns a new array does not made any changes in arrays given 
+
+// let newA = [...arr , ...arr2 , "me bhi aa gya"];
+// console.log(newA);
+// console.log(arr2);
+// console.log(arr);
+
+// using Array.from  :- creates  array 
+
+// let f = arr.find( value => value == 10);
+
+// find iterates through all values of arr 
+
+// let f = arr.find( function(value){
+//     // value == 1;
+//     if(value == 1 ){ 
+//         console.log("found");  }
+//     // else    console.log("not found");
+// });
+
+// creating array of an object 
+
+// let obj ={
+//     name : "ayush",
+//     clg:"iist"
+// }
+
+// let ar = Object.entries(obj); //[ [ 'name', 'ayush' ], [ 'clg', 'iist' ] ]
+
+// let ar = Object.keys(obj); // [ 'name', 'clg' ]
+
+// let ar = Object.values(obj) //[ 'ayush', 'iist' ]
+
+// console.log(ar);
+
+// creating array from variables 
+
+// let a=20;
+// let b=30;
+// let c=40;
+
+// let d = "ayush";
+
+// let ar = Array.of(a,b,c,d);
+// console.log(ar);
+
