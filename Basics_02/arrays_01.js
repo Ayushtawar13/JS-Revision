1cd649acbb818ea50551bb18b393bdd318d83efc
Ayushtawar13 <aayushtanwar2003@gmail.com>
2024-10-05 11:31:39 +0530

    commit arrays

---
+++ Basics_02/arrays_01.js
@@ -0,0 +1,52 @@
+// myarr = [1,2,3,4,5,6];
+// console.log(myarr);
+
+let newarr = new Array(1,2,3,4,5); // array creation usiing object 
+
+myarr1 = [1,2,3,4,5,6];
+
+// insert last push   delete last pop 
+
+// myarr1.push(7);
+// myarr1.pop();
+
+// insert at first delete at first 
+
+// myarr1.unshift(0);
+// myarr1.shift();
+
+// joining array using separaters 
+
+// let joined = myarr1.join("*"); 
+
+// console.log(myarr1);
+// console.log(joined);
+
+// slicing of array does not changes ariginal array 
+
+// console.log(myarr1.slice(1,3)); // 1 inclusive 3 excluded
+// console.log(myarr1);
+
+// // makes changes in original array
+
+// let arr = myarr1.splice(0,3); // 0 inclusive 3 inclusive
+// console.log(myarr1);
+// console.log(arr);
+
+
+// emptying an array 
+
+// let arr1 = [1,2,3,4,5,6];
+
+// let arr2 = arr1;
+
+// arr1=[];  // arr1 is empty but arr2 is pointing to the previous address of arr1
+
+// arr1.length = 0; // here both arr1 and arr2 is pointing at same address and the length of array is setted as 0 so both gives []
+
+// arr2.splice(0,arr2.length); // does the samae as above 
+
+// console.log(arr1)
+// console.log(arr2)
+
+
---
+++ Basics_02/arrays_02.js
@@ -0,0 +1,8 @@
+let arr = [1,3,5,6,7,8];
+
+let arr2 = [10,30,50,60,70,80];
+
+let arr3 = arr2.push(arr);
+
+console.log(arr);
+console.log(arr3);
