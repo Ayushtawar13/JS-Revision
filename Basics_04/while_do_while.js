81438d06e389acecb7412906c578a8a19d1ff0b4
Ayushtawar13 <aayushtanwar2003@gmail.com>
2024-10-17 12:16:17 +0530

    commit

---
+++ Basics_04/while_do_while.js
@@ -0,0 +1,11 @@
+
+let i=0;
+do{
+    console.log(`running for ${i} time`);
+    i++;
+}
+while(i<5){
+    // i=i+1;   
+    //upar sb khttm hone ke bad akhri me  
+    console.log(i+"inside while ")
+};
